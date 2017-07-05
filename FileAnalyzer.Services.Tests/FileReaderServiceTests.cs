652cc0c1e39682348f461788c4c7410507b4f591
DanielTheCritic <daniels@bytebug.co.za>
2017-07-05 19:30:16 +0200

    Created project infrastructure

---
+++ FileAnalyzer.Services.Tests/FileReaderServiceTests.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+using FileAnalyzer.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileAnalyzer.Services.Tests
+{
+    [TestFixture]
+    public class FileReaderServiceTests
+    {
+        private FileReaderService fileReaderService;
+
+        [Test]
+        public void ReadFile_GivenNullPath_ThrowsError()
+        {
+            // Assign
+
+            // Assess
+
+            // Assert
+        }
+    }
+}
